53431cc4db6c8ed785a1d676e717f53266e09e3b
Jhonathan Telenchana <diego.telenchana6241@utc.edu.ec>
2025-11-04 14:18:52 -0500

    Agregado modelo y migración de Persona

--- app/Models/Persona.php
+++ app/Models/Persona.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Persona extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'personas';
+    
+    protected $fillable = [
+        'nombre',
+        'apellido',
+        'cedula',
+        'email',
+        'telefono',
+        'direccion',
+    ];
+
+    public function adopciones()
+    {
+        return $this->hasMany(Adopcion::class);
+    }
 }
