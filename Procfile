d72b0562b61af01af57dde43712f6cf51ec50e83
Eddylucero <luceroeddy8@gmail.com>
2025-11-06 16:51:21 -0500

    Produccion 3.0

--- Procfile
+++ Procfile
@@ -1,2 +1,4 @@
 release: php artisan migrate --force && php artisan db:seed --force
-web: php artisan serve --host=0.0.0.0 --port=$PORT
+
+# Inicia el servidor web nativo de Railway (Apache/Nginx)
+web: php artisan config:clear && php artisan route:clear && php artisan view:clear && php artisan cache:clear && vendor/bin/heroku-php-apache2 public/
--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -2,16 +2,21 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User; // o tu modelo de usuarios
+use Illuminate\Support\Facades\Hash;
 
 class AdminUserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        User::updateOrCreate(
+            ['email' => 'anthony.morales4452@utc.edu.ec'],
+            [
+                'name' => 'Anthony M.',
+                'password' => Hash::make('anthoo123'),
+                'role' => 'admin', // solo si tu tabla tiene columna 'role'
+            ]
+        );
     }
 }
